bec7a7410a7372217ae6821a21e0070f3841fde0
admin <admin@gamil.com>
2019-06-20 06:50:19 +0530

    first commit

---
+++ src/main/java/com/example/testing/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.example.testing;
+
+public class HelloWorld {
+	
+	public String getMessage(boolean bigger) {
+		if (bigger) {
+			return "Hello Universe!";
+		} else {
+			return "Hello World!";
+		}
+	}
+
+}
---
+++ src/test/java/com/example/testing/HelloWorldTest.java
@@ -0,0 +1,22 @@
+package com.example.testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	private HelloWorld subject;
+
+	@Before
+	public void setup() {
+		subject = new HelloWorld();
+	}
+	
+	@Test
+	public void testGetMessage() {
+		assertEquals("Hello World!", subject.getMessage(false));
+	}
+	
+}
